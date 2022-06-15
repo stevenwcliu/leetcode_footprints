b0e097399eba91a6cbfc41f2948e7389bcd9e507
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-06-15 10:26:44 -0700

    Time: 320 ms (52.77%), Space: 15.5 MB (70.80%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 大神模版
+        # p, q = 0, n - 1 -> can't pass case: 1, 1
+        n = len(nums)
+        p, q = 0, n - 1
+        # while p < q: -> wrong
+        while p <= q:
+            # mid = p + ( q - p ) / 2 -> 4.5
+            mid = p + ( q - p ) // 2 # -> 4
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                q = mid - 1
+            else:
+                p = mid + 1
+        
+        return -1
+        
