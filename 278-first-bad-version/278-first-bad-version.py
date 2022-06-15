35722a49d441440cf92f65640dd4b9778d346f2d
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-06-15 10:20:44 -0700

    Time: 50 ms (29.46%), Space: 13.9 MB (12.04%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -4,8 +4,8 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         # 大神模版
-        # p, q = 0, n - 1
-        p, q = 1, n
+        # p, q = 0, n - 1 -> can't pass case: 1, 1
+        p, q = 0, n
         while p < q:
             # mid = p + ( q - p ) / 2 -> 4.5
             mid = p + ( q - p ) // 2 # -> 4
