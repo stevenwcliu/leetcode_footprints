4c2ca9cec51bcea305fc9691e293baafdf60b664
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-06-17 08:36:29 -0700

    Time: 103 ms (67.09%), Space: 15.6 MB (9.96%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,28 @@
 class Solution:
-    # grok sol
+    # 云神
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-
-        res = [- 1, -1]
-        res[0] = self.binary_search(nums, target, False)
-        if res[0] != -1:  # no need to search, if 'key' is not present in the input array
-            res[1] = self.binary_search(nums, target, True)
-        return res
-
-
-    # modified Binary Search
-    def binary_search(self, arr, key, findMaxIndex):
-        keyIndex = -1
-        start, end = 0, len(arr) - 1
-        while start <= end:
-            mid = start + (end - start) // 2
-            if key < arr[mid]:
-                end = mid - 1
-            elif key > arr[mid]:
-                start = mid + 1
-            else:  # key == arr[mid]
-                keyIndex = mid
-                if findMaxIndex:
-                    start = mid + 1  # search ahead to find the last index of 'key'
-                else:
-                    end = mid - 1  # search behind to find the first index of 'key'
-        return keyIndex
+        n = len(nums)
+        p, q = 0, n - 1
+        
+        if n == 0:
+            # wrong: return {-1, -1}
+            return [-1, -1]
+        
+        while p < q:
+            mid = p + (q - p) // 2
+            if nums[mid] < target:
+                p = mid + 1
+            else:
+                q = mid
+        
+        if nums[p] != target:
+            return [-1, -1]
+        p0 = p
+        q = n - 1
+        while p < q:
+            mid = p + (q - p) // 2
+            if nums[mid + 1] > target:
+                q = mid
+            else:
+                p = mid + 1
+        return [p0, p]
