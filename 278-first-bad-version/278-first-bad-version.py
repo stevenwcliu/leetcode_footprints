a902a567b5f780a74db12f456e1a724ac751e5ff
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-06-23 09:24:45 -0700

    Time: 60 ms (11.89%), Space: 13.8 MB (96.70%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,16 +3,26 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        # 大神模版
-        # p, q = 0, n - 1 -> can't pass case: 1, 1
-        p, q = 0, n
+        # return the first bad version
+        # tc 1: [0,0,0,1,1,1]
+        # output: 3(?)
+        
+        # Clarifications:
+        # at least 1 bad?
+        # size of n?
+        
+        # return lower bound
+        # p, q = 0, n - 1
+        p, q = 1, n # according to constraint
+        
         while p < q:
-            # mid = p + ( q - p ) / 2 -> 4.5
-            mid = p + ( q - p ) // 2 # -> 4
-            if isBadVersion(mid):
+            mid = p + (q - p) // 2 
+            if isBadVersion(mid): # search the left half
                 q = mid
             else:
                 p = mid + 1
         
         return p
+                
+        
         
