765b98c60a0bdab69f6e7d360459e78fe667c6bc
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-07-06 22:35:02 -0700

    Time: 332 ms (26.99%), Space: 33.2 MB (14.59%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,56 +1,57 @@
 # nc
+# hand-typed
+
 class TrieNode:
     def __init__(self):
         self.children = [None] * 26
         self.end = False
-        
+
 class Trie:
-    
+
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        '''
+        initialize the data structure
+        '''
         self.root = TrieNode()
         
-
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
+        '''
+        insert a word into the trie
+        '''
         curr = self.root
         for c in word:
-            i = ord(c)-ord("a")
+            i = ord(c) - ord('a')
             if curr.children[i] == None:
                 curr.children[i] = TrieNode()
             curr = curr.children[i]
         curr.end = True
-        
+
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
+        '''
+        returns if the word is in the trie
+        '''
         curr = self.root
         for c in word:
-            i = ord(c)-ord("a")
+            # i = ord('c') - ord('a')
+            i = ord(c) - ord('a')
             if curr.children[i] == None:
                 return False
-            curr = curr.children[i]
-        return curr.end
+            curr = curr.children[i] # missing
+        return curr.end # !! reason of not just return True: ??
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
+        '''
+        returns if there is any word in the trie starts with the given prefix
+        '''
+        
         curr = self.root
         for c in prefix:
-            i = ord(c)-ord("a")
+            i = ord(c) - ord('a')
             if curr.children[i] == None:
                 return False
             curr = curr.children[i]
         return True
         
-
-
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
