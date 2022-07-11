6fbc278678e8c76e1a4ba5c3770ea4205d8d62bb
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-07-11 13:10:38 -0700

    Time: 199 ms (83.61%), Space: 14.5 MB (49.38%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -0,0 +1,22 @@
+# ref: https://courses.codepath.org/courses/advanced_software_eng/pages/warmup_solutions/warmup_soln_6
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color = {}
+        for i in range(len(graph)):
+            if i not in color:
+                color[i] = 0
+                if not self.dfs(i, graph, color):
+                    return False
+        return True
+
+    def dfs(self, pos, graph, color):
+        for i in graph[pos]:
+            if i in color:
+                if color[i] == color[pos]:
+                    return False
+            else:
+                color[i] = 1 - color[pos]
+                if not self.dfs(i, graph, color):
+                    return False
+        return True
