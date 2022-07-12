701632c112a5f4e41c1602281f34b6bf22deb8b8
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-07-11 18:02:02 -0700

    Time: 923 ms (49.07%), Space: 20.1 MB (63.71%) - LeetHub

--- 642-design-search-autocomplete-system/642-design-search-autocomplete-system.py
+++ 642-design-search-autocomplete-system/642-design-search-autocomplete-system.py
@@ -1,57 +1,68 @@
-# discussion post: https://leetcode.com/problems/design-search-autocomplete-system/discuss/105386/Python-Clean-Solution-Using-Trie
-class TrieNode(object):
+# discussion: https://leetcode.com/problems/design-search-autocomplete-system/discuss/460803/Python-Trie-Solution-easy-to-understand-with-explanation
+
+class TrieNode():
     def __init__(self):
-        self.children = {}
         self.isEnd = False
-        self.data = None
-        self.rank = 0
-        
+        self.children = {}
+        self.hot = 0
+    
 class AutocompleteSystem(object):
     def __init__(self, sentences, times):
         self.root = TrieNode()
-        self.keyword = ""
+        self.searchTerm = ""
+        # 1. add historical data
         for i, sentence in enumerate(sentences):
-            self.addRecord(sentence, times[i])
-
-    def addRecord(self, sentence, hot):
-        p = self.root
-        for c in sentence:
-            if c not in p.children:
-                p.children[c] = TrieNode()
-            p = p.children[c]
-        p.isEnd = True
-        p.data = sentence
-        p.rank -= hot
-    
-    def dfs(self, root):
-        ret = []
-        if root:
-            if root.isEnd:
-                ret.append((root.rank, root.data))
-            for child in root.children:
-                ret.extend(self.dfs(root.children[child]))
-        return ret
+            self.add(sentence, times[i])
+			
+    def add(self,sentence, hot):
+        node = self.root
+        #2. for each character in sentence
+        for c in sentence: 
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        #3. when last character is added,
+        #   make node.isEnd = True indicate that the current node is end of the sentence
+        node.isEnd = True
+        #4. do -= because by negating, we can sort as ascending order later
+        node.hot-= hot
         
-    def search(self, sentence):
-        p = self.root
-        for c in sentence:
-            if c not in p.children:
-                return []
-            p = p.children[c]
-        return self.dfs(p)
-    
+    def search(self):
+        node = self.root
+        res = []
+        path = ""
+
+        for c in self.searchTerm:
+            if c not in node.children:
+                return res
+            # 6. add each character to path variable, path will added to res when we found node.isEnd ==True
+            path +=c
+            node = node.children[c]
+        # 7. at this point, node is at the given searchTerm.
+        # for ex. if search term is "i_a", we are at "a" node.
+        # from this point, we need to search all the possible sentence by using DFS
+        self.dfs(node, path,res)
+        # 11. variable res has result of all the relevant sentences
+        # we just need to do sort and return [1] element of first 3
+        return [item[1] for item in sorted(res)[:3]]
+            
+    def dfs(self,node, path, res):
+        # 8. Check if node is end of the sentence
+        # if so, add path to res
+        if node.isEnd:
+            # 9. when add to res, we also want to add hot for sorting
+            res.append((node.hot,path))
+        # 10. keep going if the node has child
+        # until there is no more child (reached to bottom)
+        for c in node.children:
+            self.dfs(node.children[c], path+c,res)
+
     def input(self, c):
-        results = []
         if c != "#":
-            self.keyword += c
-            results = self.search(self.keyword)
-        else:
-            self.addRecord(self.keyword, 1)
-            self.keyword = ""
-        return [item[1] for item in sorted(results)[:3]]
+            # 5. if input is not "#" add c to self.searchTerm and do self.search each time
+            self.searchTerm +=c
+            return self.search()
         
-
-
-# Your AutocompleteSystem object will be instantiated and called as such:
-# obj = AutocompleteSystem(sentences, times)
-# param_1 = obj.input(c)
+        else:
+            self.add(self.searchTerm, 1)
+            self.searchTerm =""
