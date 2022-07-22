cec9753e5928421786f1c484e98a171bde4d8882
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-07-21 21:06:21 -0700

    Time: 280 ms (21.15%), Space: 18 MB (98.68%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,5 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Jul 21 cp in-class mock
+        
+        # by 城主思路
+        # TC: O(nlgn), nlgn for sorting
+        # SC: O(n)
         
         # corner cases
         if intervals is None or len(intervals) == 0:
@@ -8,18 +13,22 @@ class Solution:
         # practice-needed
         # sort by the start val i[0] of interval i
         intervals.sort(key = lambda i : i[0])
-        res = [] # initialized with the first element to avoid an edge case??
+        res = []
         
         cur = intervals[0]
         
-        for next in intervals:
-            if cur[1] >= next[0]:
-                cur[1] = max(cur[1], next[1])
+        # intervals[1:]: next starts from the second interval makes more sense
+        for next in intervals[1:]: 
+            # print("cur: ",cur)
+            # print("next: ",next)
+            if cur[1] >= next[0]: # cur.end >= next.start: overlapped
+                cur[1] = max(cur[1], next[1]) 
+                # without max() will fail [[1,4],[2,3]]
             else:
-                res.append(cur)
-                cur = next
+                res.append(cur) # insert a new interval
+                cur = next 
             
-        res.append(cur)
+        res.append(cur) # will miss the last interval without this line
             
         return res
         
