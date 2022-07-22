188423d56ad8b8cab20f8bfbac21ad8fbebcd59c
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-07-22 16:23:29 -0700

    Time: 189 ms (69.77%), Space: 18 MB (84.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,34 +1,28 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # Jul 21 cp in-class mock
         
-        # by 城主思路
-        # TC: O(nlgn), nlgn for sorting
-        # SC: O(n)
+        # Jul 22 2nd review
+        # TC: O(nlogn), sorting
+        # SC: O(n), worst case: no overlapping
         
-        # corner cases
+        # edge case
         if intervals is None or len(intervals) == 0:
             return []
         
-        # practice-needed
-        # sort by the start val i[0] of interval i
+        # sort by start
         intervals.sort(key = lambda i : i[0])
+        
         res = []
         
         cur = intervals[0]
         
-        # intervals[1:]: next starts from the second interval makes more sense
-        for next in intervals[1:]: 
-            # print("cur: ",cur)
-            # print("next: ",next)
-            if cur[1] >= next[0]: # cur.end >= next.start: overlapped
-                cur[1] = max(cur[1], next[1]) 
-                # without max() will fail [[1,4],[2,3]]
+        for next in intervals[1:]:
+            # overlap
+            if cur[1] >= next[0]: # cur.end >= next.start
+                cur[1] = max(cur[1], next[1]) # max() is important
             else:
-                res.append(cur) # insert a new interval
-                cur = next 
-            
-        res.append(cur) # will miss the last interval without this line
-            
-        return res
+                res.append(cur)
+                cur = next
         
+        res.append(cur) # add the last interval
+        return res
