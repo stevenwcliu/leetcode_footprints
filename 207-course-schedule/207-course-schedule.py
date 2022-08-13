57a587aabe836690a691e7469246caac3a331f60
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-08-12 17:23:40 -0700

    Time: 125 ms (75.33%), Space: 15.4 MB (89.42%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -2,18 +2,32 @@
 # SC O(N)
 # fuxuemingzhu 方法一：拓扑排序，BFS
 
+from collections import defaultdict
+
 class Solution(object):
+    '''
     def canFinish(self, N, prerequisites):
         """
         :type N,: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        graph = collections.defaultdict(list)
-        indegrees = collections.defaultdict(int)
+        graph = defaultdict(list) # courseDict
+        indegrees = defaultdict(int)
+        
         for u, v in prerequisites:
-            graph[v].append(u)
+            graph[v].append(u) # u: next course, v: prev course
             indegrees[u] += 1
+            
+        print("graph: ", graph) # {0: [1]}
+        print("indegrees: ", indegrees) # {1:1}
+        
+        # build graph - ref from nc
+        # preMap = { i:[] for i in range(N)} 
+        # for crs, pre in prerequisites:
+        #     preMap[crs].append(pre)
+        # print("preMap: ", preMap) # {0: [], 1: [0]}
+        
         for i in range(N):
             zeroDegree = False
             for j in range(N):
@@ -25,3 +39,31 @@ class Solution(object):
             for node in graph[j]:
                 indegrees[node] -= 1
         return True
+    
+    '''
+
+    # 103 w7 warm-up
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        ad_list = {i: set() for i in range(numCourses)}
+        degrees = [0] * numCourses
+        for prereq, course in prerequisites:
+            ad_list[prereq].add(course)
+            degrees[course] += 1
+        
+        eligibleCourses = []
+        for course in range(numCourses):
+            if degrees[course] == 0:
+                eligibleCourses.append(course)
+        
+        i = 0
+        while i < len(eligibleCourses):
+            next_course = eligibleCourses[i]
+
+            for course in ad_list[next_course]:
+                degrees[course] -= 1
+                if degrees[course] == 0:
+                    eligibleCourses.append(course)
+            
+            i += 1
+        
+        return len(eligibleCourses) == numCourses
