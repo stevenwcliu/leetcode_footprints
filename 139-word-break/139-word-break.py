7a33941734f841707615f3910654344d005f4522
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-09-01 17:27:24 -0700

    Time: 52 ms (70.50%), Space: 13.9 MB (94.73%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,15 +1,34 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # nc
-        # https://youtu.be/Sx9NNgInc3A
+#         # nc
+#         # https://youtu.be/Sx9NNgInc3A
+#         dp = [False] * (len(s) + 1)
+#         dp[len(s)] = True
+
+#         for i in range(len(s) - 1, -1, -1):
+#             for w in wordDict:
+#                 if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+#                     dp[i] = dp[i + len(w)]
+#                 if dp[i]:
+#                     break
+
+#         return dp[0]
+
+        # fxmz
+
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        # print(s)
+        # print(wordDict)
         dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
-
-        for i in range(len(s) - 1, -1, -1):
-            for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]
-                if dp[i]:
-                    break
-
-        return dp[0]
+        dp[0] = True
+        # print(dp)
+        for i in range(1, len(s) + 1):
+            for k in range(i):
+                if dp[k] and s[k:i] in wordDict:
+                    dp[i] = True
+                    # print(dp)
+        return dp[-1]
