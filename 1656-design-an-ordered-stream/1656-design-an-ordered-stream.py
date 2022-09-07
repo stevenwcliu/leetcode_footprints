50f4b1a87c27c765a6cbd91b976122c1c22c00a0
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-09-06 22:20:24 -0700

    Time: 481 ms (9.60%), Space: 14.8 MB (48.66%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,17 @@
+class OrderedStream:
+    # ref: https://leetcode.cn/problems/design-an-ordered-stream/solution/pythonjavatypescriptgo-by-himymben-ne0f/
+
+
+    def __init__(self, n: int):
+        self.data = [""] * (n + 1)
+        self.ptr = 1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.data[idKey] = value
+        ans = []
+        while self.ptr < len(self.data) and self.data[self.ptr]:
+            ans.append(self.data[self.ptr])
+            self.ptr += 1
+        return ans
+
+
