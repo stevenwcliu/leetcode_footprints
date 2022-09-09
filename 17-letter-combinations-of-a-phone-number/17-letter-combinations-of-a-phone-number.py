5696e2806f872efeb7cd79f806c06e43a7d53fc3
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-09-08 17:55:35 -0700

    Time: 63 ms (12.83%), Space: 13.9 MB (31.69%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # ref: algo monster
+        if len(digits) == 0: 
+            return []
+        
+        KEYBOARD = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+
+        def dfs(self, path, res):
+            if len(path) == len(digits):
+                res.append(''.join(path))
+                return
+
+            next_number = digits[len(path)]
+            for letter in KEYBOARD[next_number]:
+                path.append(letter)
+                dfs(self, path, res)
+                path.pop()
+
+        res = []
+        dfs(self, [], res)
+        return res
