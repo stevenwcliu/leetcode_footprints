7bc00f2d25b5ed986fde0917c73eb04d8c1a05e1
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-10-05 10:18:46 -0700

    Time: 58 ms (39.44%), Space: 13.9 MB (72.30%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,22 +1,24 @@
-# nc updated
+# nc updated with comments from lc sol
+# oct 5, 2022
+# grind75
 class Solution:
     def isValid(self, s: str) -> bool:
         # corner case: odd num chars
-        
-        if len(s) % 2 == 1:
-            return False
+        # if len(s) % 2 == 1:
+        #     return False
 
-        Map = {")": "(", "]": "[", "}": "{"}
-        stack = []
+        mapping = {")": "(", "]": "[", "}": "{"} # use to check if the incoming close bracket matches with the existed open bracket in the stack 
+        stack = [] # keep track of opening brackets
 
         for c in s:
-            if c not in Map:
+            if c not in mapping:
                 stack.append(c)
                 continue
-            if not stack or stack[-1] != Map[c]:
+            if not stack or stack[-1] != mapping[c]:
                 return False
-            stack.pop()
-
+            stack.pop() # the element at the top of the stack is an opening bracket of the same type, then we pop it off the stack and continue processing
+            
+        # In the end, if the stack is empty, then we have a valid expression.
         return not stack
                 
         
