ba78dd8cc628be4edb7d5785779b5078f3e541db
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2022-10-11 20:36:44 -0700

    Time: 336 ms (68.11%), Space: 14.7 MB (92.16%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -3,13 +3,15 @@ class OrderedStream:
 
 
     def __init__(self, n: int):
-        self.data = [""] * (n + 1)
+        # self.data = [""] * (n + 1) # list 只能从0 - n; 但ptr 有可能到 n + 1
+        self.data = {}
         self.ptr = 1
 
     def insert(self, idKey: int, value: str) -> List[str]:
         self.data[idKey] = value
         ans = []
-        while self.ptr < len(self.data) and self.data[self.ptr]:
+        # while self.ptr < len(self.data) and self.data[self.ptr]:
+        while self.ptr in self.data:
             ans.append(self.data[self.ptr])
             self.ptr += 1
         return ans
